058a87d93c8fb85eb08d42f0be2970a739328a55
Ezra Vance <3zravance@gmail.com>
2020-09-20 21:06:38 -0400

    PlayerService methods defined.

--- arcade/src/main/java/com/ezra/arcade/service/PlayerService.java
+++ arcade/src/main/java/com/ezra/arcade/service/PlayerService.java
@@ -1,30 +1,38 @@
 package com.ezra.arcade.service;
 
 import com.ezra.arcade.model.Player;
+import com.ezra.arcade.repository.PlayerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PlayerService {
 
-    public List<Player> getAllPlayers(){
+    @Autowired
+    private PlayerRepository playerRepository;
 
+    public List<Player> getAllPlayers(){
+        List<Player> players = new ArrayList<>();
+        playerRepository.findAll().forEach(players::add);
+        return players;
     }
 
     public Player getPlayer(Integer id){
-
+        return playerRepository.findById(id).get();
     }
 
     public void addPlayer(Player player) {
-
+        playerRepository.save(player);
     }
 
     public void updatePlayer(Player player, Integer id) {
-
+        playerRepository.save(player);
     }
 
     public void deletePlayer(Integer id) {
-
+        playerRepository.deleteById(id);
     }
 }
