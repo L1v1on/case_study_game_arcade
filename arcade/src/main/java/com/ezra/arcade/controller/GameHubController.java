f4d9bfd0baba1b880270e63f2ca750d26e93f952
Ezra Vance <3zravance@gmail.com>
2020-09-24 18:39:32 -0400

    ugly css created.

--- arcade/src/main/java/com/ezra/arcade/controller/GameHubController.java
+++ arcade/src/main/java/com/ezra/arcade/controller/GameHubController.java
@@ -1,4 +1,13 @@
 package com.ezra.arcade.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GameHubController {
+
+    @RequestMapping("/gameHub")
+    public String showGameHubPage(){
+        return "gameHub";
+    }
 }
